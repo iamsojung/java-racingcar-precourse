1d042450d156429611bf48961b0db2ddbec0f26c
iamsojung <sojungkid@gmail.com>
2022-10-08 15:17:19 +0900

    feat: 레이싱 회수 입력 기능/테스트

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -2,16 +2,19 @@ package racingcar.controller;
 
 import racingcar.exception.IllegalArgumentException;
 import racingcar.model.CarNames;
+import racingcar.model.Round;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingCarGameController {
     public static final String REQUEST_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (각 자동차는 5글자 이하 쉼표(,) 기준으로 입력해주세요.)";
+    public static final String REQUEST_INPUT_ROUND = "시도할 회수는 몇회인가요?";
 
     private final OutputView outputView = new OutputView();
-
+    private final Round round;
     public RacingCarGameController() {
         CarNames names = getValidCarNames();
+        this.round = validRound();
     }
 
     public void run() {
@@ -33,6 +36,29 @@ public class RacingCarGameController {
     private boolean checkCarNamesUserInput(String userinput) {
         try {
             new CarNames(userinput);
+            return true;
+        } catch (IllegalArgumentException e) {
+            outputView.error(e);
+            return false;
+        }
+    }
+
+    private Round validRound () {
+        boolean isValid;
+        String userInput;
+
+        do {
+            outputView.notice(REQUEST_INPUT_ROUND);
+            userInput = InputView.getUserInput();
+            isValid = checkRoundUserInput(userInput);
+        } while (!isValid);
+
+        return new Round(userInput);
+    }
+
+    private boolean checkRoundUserInput(String userinput) {
+        try {
+            new Round(userinput);
             return true;
         } catch (IllegalArgumentException e) {
             outputView.error(e);
