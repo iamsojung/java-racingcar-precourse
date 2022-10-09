b9dcf245f21228bc426ebf30299ec4a24607db88
iamsojung <sojungkid@gmail.com>
2022-10-10 01:24:04 +0900

    feat: 플레이어,플레이어그룹 기능

---
+++ src/main/java/racingcar/model/Player.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Player {
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
+
+    private final Car car;
+
+    public Player(Car car) {
+        this.car = car;
+    }
+
+    public Car getCar() {return car;}
+
+    public int createMovementScore() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+}
