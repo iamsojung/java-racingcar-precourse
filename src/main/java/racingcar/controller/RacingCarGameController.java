494134f92e71bcb125972ef3a02fcbbdffc281f8
iamsojung <sojungkid@gmail.com>
2022-10-10 01:28:56 +0900

    feat: controller 게임 실행 기능

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,7 +1,11 @@
 package racingcar.controller;
 
 import racingcar.exception.IllegalArgumentException;
+import racingcar.model.Car;
+import racingcar.model.CarName;
 import racingcar.model.CarNames;
+import racingcar.model.Player;
+import racingcar.model.PlayerGroup;
 import racingcar.model.Round;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -9,15 +13,31 @@ import racingcar.view.OutputView;
 public class RacingCarGameController {
     public static final String REQUEST_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (각 자동차는 5글자 이하 쉼표(,) 기준으로 입력해주세요.)";
     public static final String REQUEST_INPUT_ROUND = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_NOTICE_STR = "실행 결과";
 
     private final OutputView outputView = new OutputView();
+    private final PlayerGroup group;
     private final Round round;
+
     public RacingCarGameController() {
         CarNames names = getValidCarNames();
         this.round = validRound();
+        this.group = new PlayerGroup();
+
+        for (CarName name : names.getCarNames()) {
+            Player player = new Player(new Car(name));
+            group.append(player);
+        }
     }
 
     public void run() {
+        outputView.notice(RESULT_NOTICE_STR);
+
+        while(!round.isFinished()) {
+            round.playRound();
+            group.runARace();
+            outputView.notice(group.toStringCarDistanceByPlayer());
+        }
     }
 
     private CarNames getValidCarNames() {
