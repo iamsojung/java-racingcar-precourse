d05c6bc478a649c374cfd5e37a645df6abb1ab65
iamsojung <sojungkid@gmail.com>
2022-10-10 13:27:05 +0900

    refactor: winners 테스트케이스 수정

--- src/test/java/racingcar/model/WinnersTest.java
+++ src/test/java/racingcar/model/WinnersTest.java
@@ -18,18 +18,18 @@ public class WinnersTest {
     @DisplayName("최종 우승자 출력 값 확인")
     @Test
     void 최종_우승자_출력값_확인_테스트() {
-        Player aaplayer = new Player(new Car(new CarName("aaplayer")));
-        Player bbplayer = new Player(new Car(new CarName("bbplayer")));
+        Player apple = new Player(new Car(new CarName("apple")));
+        Player kiwi = new Player(new Car(new CarName("kiwi")));
 
-        aaplayer.getCar().run(5);
-        bbplayer.getCar().run(3);
+        apple.getCar().run(5);
+        kiwi.getCar().run(3);
 
-        playerGroup.append(aaplayer);
-        playerGroup.append(bbplayer);
+        playerGroup.append(apple);
+        playerGroup.append(kiwi);
 
         Winners winner = new Winners(playerGroup);
 
-        String winners = "최종 우승자: red";
+        String winners = "최종 우승자 : apple";
         assertEquals(winners, winner.toStringFinalWinnerNames());
     }
 }
